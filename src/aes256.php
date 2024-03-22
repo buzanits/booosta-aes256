9e03b039bc155fae405316472123d2b8b6af1b50
Peter Buzanits <p.buzanits@epb.at>
2024-03-22 18:03:23 +0100

    use retrieve_key function

--- src/aes256.php
+++ src/aes256.php
@@ -18,8 +18,8 @@ class aes256 extends \booosta\crypter\Crypter
     $this->check_install(false);
 
     if($key !== null) $this->key = $key;
-    elseif(!is_readable($this->config('aes256_keyfile'))) $this->key = false;
-    else include($this->config('aes256_keyfile'));
+    #elseif(!is_readable($this->config('aes256_keyfile'))) $this->key = false;
+    #else include($this->config('aes256_keyfile'));
   }
 
   public function set_key($data) { $this->key = $data; }
@@ -28,18 +28,25 @@ class aes256 extends \booosta\crypter\Crypter
 
   public function encrypt($plaintext)
   {
-    if($this->key === false) return false;
+    if($this->retrieve_key() === null) return false;
     if(!$this->check_install()) return null;
  
-    return base64_encode(openssl_encrypt($plaintext, $this->cipher, base64_decode($this->key), 0, $this->iv));
+    return base64_encode(openssl_encrypt($plaintext, $this->cipher, base64_decode($this->retrieve_key()), 0, $this->iv));
   }
 
   public function decrypt($ciphertext)
   {
-    if($this->key === false) return false;
+    if($this->retrieve_key() === null) return false;
     if(!$this->check_install()) return null;
  
-    return openssl_decrypt(base64_decode($ciphertext), $this->cipher, base64_decode($this->key), 0, $this->iv);
+    return openssl_decrypt(base64_decode($ciphertext), $this->cipher, base64_decode($this->retrieve_key()), 0, $this->iv);
+  }
+
+  protected function retrieve_key()
+  {
+    if($this->key) return $this->key;
+    if(is_readable($this->config('aes256_keyfile'))) include($this->config('aes256_keyfile'));
+    return $key;
   }
 
   protected function check_install($check_cipher = true)
